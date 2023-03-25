944c059711de0392ad5673c27e5616bb4399ebd1
Callum <csykes@structurecraft.com>
2023-03-25 15:10:45 -0700

    Moved wget outside of try/except

--- py/download-yak.py
+++ py/download-yak.py
@@ -5,14 +5,6 @@ if os.path.isfile('yak.exe'):
     print('Yak is already downloaded. Using Cached version')
     sys.exit(0)
 
-try:
-    import wget
-    url = 'http://files.mcneel.com/yak/tools/latest/yak.exe'
-    wget.download(url, 'yak.exe')
-
-    sys.exit(0)
-
-except:
-    import sys
-    print ('failed to download yak from McNeel Servers')
-    sys.exit(1)
+import wget
+url = 'http://files.mcneel.com/yak/tools/latest/yak.exe'
+wget.download(url, 'yak.exe')
